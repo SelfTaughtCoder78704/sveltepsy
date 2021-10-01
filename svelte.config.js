790d966ece7e6fb3573bf727680b444ddae9f7c8
SelfTaughtCoder78704 <bobbynicholson78704@gmail.com>
2021-10-01 15:02:18 -0500

    trying

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,8 @@ import adapter from "@sveltejs/adapter-netlify";
 
 const config = {
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
+    adapter: adapter(), // currently the adapter does not take any options
     target: "#svelte",
-    adapter: adapter(),
   },
 };
 
